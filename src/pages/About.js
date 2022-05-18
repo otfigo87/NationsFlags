b35e572a2916e76b3abb744a0e2646b7504258a7
Otmane Aatik <aaotmane@gmail.com>
2022-05-18 17:12:21 -0400

    structure

---
+++ src/pages/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />  
+      <h1>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+        quod autem asperiores quis perspiciatis hic id, illum dolorem quisquam
+        quasi adipisci animi voluptatem, ab eum provident officiis deleniti aut
+        debitis voluptate sint itaque reiciendis ipsam? Voluptas, possimus at
+        quaerat laboriosam sint nostrum libero totam. Aspernatur, quae beatae
+        soluta perferendis repellendus doloremque velit ipsa eveniet, laudantium
+        non dignissimos pariatur expedita voluptate aliquid. Impedit atque
+        consequuntur modi numquam ullam nostrum consequatur aliquam, quis cumque
+        commodi itaque aut facere delectus ipsam veniam quod?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime libero
+        odit pariatur ex atque, cumque possimus dignissimos, modi reiciendis
+        magni quidem est consequuntur consequatur odio voluptate quas dolore?
+        Nam illum enim architecto minima magnam accusamus dolorem fugit
+        deserunt, libero et temporibus harum, aut iusto? Aut eius exercitationem
+        tempore quas sunt tempora soluta modi beatae natus odit aspernatur
+        assumenda sapiente quae, deserunt, reprehenderit sint qui, in adipisci
+        quasi voluptatem sequi nobis.
+      </p>
+    </div>
+  );
+};
+
+export default About;
